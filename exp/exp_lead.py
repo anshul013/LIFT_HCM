da2df93649c6b11602b73e4cdddeb2f5956e3cce
anshul013 <ultimatenanshul@gmail.com>
2025-07-14 10:30:13 +0200

    Modified exp_lead.py

--- exp/exp_lead.py
+++ exp/exp_lead.py
@@ -1,5 +1,4 @@
 
-from exp.exp_main import Exp_Main
 from exp.exp_hcm import Exp_HCM
 from models import LIFT
 
@@ -7,28 +6,22 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-class Exp_Lead(Exp_Main):
+class Exp_Lead(Exp_HCM):
     def __init__(self, args):
-        # Use Exp_HCM as parent for HCM models
-        if args.model in ['TSMixerH', 'TMixerH']:
-            Exp_HCM.__init__(self, args)
-        else:
-            super(Exp_Lead, self).__init__(args)
+        super(Exp_Lead, self).__init__(args)
 
     def _get_data(self, flag, **kwargs):
         return super()._get_data(flag, prefetch_path=f'{self.args.prefetch_path}_{flag}.npz',
-                                 leader_num=self.args.leader_num,
-                                 prefetch_batch_size=self.args.prefetch_batch_size,
-                                 variable_batch_size=self.args.variable_batch_size,
-                                 efficient=self.args.efficient, local_max=True,
-                                 pin_gpu=self.args.pin_gpu,
-                                 pred_path=self.args.pred_path if hasattr(self.args, 'pred_path') else None,
-                                 **kwargs)
+                                leader_num=self.args.leader_num,
+                                prefetch_batch_size=self.args.prefetch_batch_size,
+                                variable_batch_size=self.args.variable_batch_size,
+                                efficient=self.args.efficient, local_max=True,
+                                pin_gpu=self.args.pin_gpu,
+                                pred_path=self.args.pred_path if hasattr(self.args, 'pred_path') else None,
+                                **kwargs)
 
     def _build_model(self, model=None, framework_class=None):
         if self.args.lift and 'LightMTS' not in self.args.model:
             framework_class = LIFT.Model
-        # Use appropriate parent's _build_model
-        parent = Exp_HCM if self.args.model in ['TSMixerH', 'TMixerH'] else Exp_Main
-        model = parent._build_model(self, model, framework_class=framework_class)
+        model = super()._build_model(model, framework_class=framework_class)
         return model
