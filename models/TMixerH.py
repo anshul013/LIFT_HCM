86c6f1f0b69ca515db763c58af4b80ae1e837b37
anshul013 <ultimatenanshul@gmail.com>
2025-07-14 16:30:50 +0200

    Changed model files and seed back to 2023

--- models/TMixerH.py
+++ models/TMixerH.py
@@ -9,7 +9,7 @@ class Model(_TMixerH):
         
     def load_state_dict(self, state_dict, strict=True):
         # Call parent's load_state_dict
-        super().load_state_dict(state_dict, strict)
+        result = super().load_state_dict(state_dict, strict)
         
         # Re-initialize cluster models if needed
         if not hasattr(self, 'cluster_models') or len(self.cluster_models) == 0:
@@ -37,51 +37,53 @@ class Model(_TMixerH):
                     
                     # Add to ModuleList
                     self.cluster_models.append(mixer_block)
-                    
-    def requires_grad_(self, requires_grad: bool = True):
-        # If we're using LIFT and freezing, only freeze the backbone
-        if hasattr(self.args, 'lift') and self.args.lift and not requires_grad:
-            # First freeze everything
-            for param in self.parameters():
-                param.requires_grad = False
+        
+        return result
+        
+    # def requires_grad_(self, requires_grad: bool = True):
+    #     # If we're using LIFT and freezing, only freeze the backbone
+    #     if hasattr(self.args, 'lift') and self.args.lift and not requires_grad:
+    #         # First freeze everything
+    #         for param in self.parameters():
+    #             param.requires_grad = False
                 
-            # Then unfreeze specific LIFT components if they exist
-            if hasattr(self, 'lead_refiner'):
-                # Unfreeze temperature parameter for leader selection
-                if hasattr(self.lead_refiner, 'temperature'):
-                    self.lead_refiner.temperature.requires_grad = True
+    #         # Then unfreeze specific LIFT components if they exist
+    #         if hasattr(self, 'lead_refiner'):
+    #             # Unfreeze temperature parameter for leader selection
+    #             if hasattr(self.lead_refiner, 'temperature'):
+    #                 self.lead_refiner.temperature.requires_grad = True
                     
-                # Unfreeze mix_layer (ComplexLinear) parameters
-                if hasattr(self.lead_refiner, 'mix_layer'):
-                    for param in self.lead_refiner.mix_layer.parameters():
-                        param.requires_grad = True
+    #             # Unfreeze mix_layer (ComplexLinear) parameters
+    #             if hasattr(self.lead_refiner, 'mix_layer'):
+    #                 for param in self.lead_refiner.mix_layer.parameters():
+    #                     param.requires_grad = True
                         
-                # Unfreeze FilterFactory parameters
-                if hasattr(self.lead_refiner, 'factory'):
-                    factory = self.lead_refiner.factory
+    #             # Unfreeze FilterFactory parameters
+    #             if hasattr(self.lead_refiner, 'factory'):
+    #                 factory = self.lead_refiner.factory
                     
-                    # Unfreeze classifier, basic_state, and bias if they exist
-                    if factory.num_state > 1 and factory.need_classifier:
-                        if hasattr(factory, 'classifier'):
-                            for param in factory.classifier.parameters():
-                                param.requires_grad = True
-                        if hasattr(factory, 'basic_state'):
-                            factory.basic_state.requires_grad = True
-                        if hasattr(factory, 'bias'):
-                            factory.bias.requires_grad = True
+    #                 # Unfreeze classifier, basic_state, and bias if they exist
+    #                 if factory.num_state > 1 and factory.need_classifier:
+    #                     if hasattr(factory, 'classifier'):
+    #                         for param in factory.classifier.parameters():
+    #                             param.requires_grad = True
+    #                     if hasattr(factory, 'basic_state'):
+    #                         factory.basic_state.requires_grad = True
+    #                     if hasattr(factory, 'bias'):
+    #                         factory.bias.requires_grad = True
                     
-                    # Unfreeze mix_head parameters
-                    if factory.num_state == 1:
-                        if hasattr(factory, 'mix_head'):
-                            for param in factory.mix_head.parameters():
-                                param.requires_grad = True
-                    else:
-                        if hasattr(factory, 'mix_head_w'):
-                            factory.mix_head_w.requires_grad = True
-                        if hasattr(factory, 'mix_head_b'):
-                            factory.mix_head_b.requires_grad = True
-        else:
-            # Otherwise use normal freezing behavior
-            for param in self.parameters():
-                param.requires_grad = requires_grad
-        return self 
+    #                 # Unfreeze mix_head parameters
+    #                 if factory.num_state == 1:
+    #                     if hasattr(factory, 'mix_head'):
+    #                         for param in factory.mix_head.parameters():
+    #                             param.requires_grad = True
+    #                 else:
+    #                     if hasattr(factory, 'mix_head_w'):
+    #                         factory.mix_head_w.requires_grad = True
+    #                     if hasattr(factory, 'mix_head_b'):
+    #                         factory.mix_head_b.requires_grad = True
+    #     else:
+    #         # Otherwise use normal freezing behavior
+    #         for param in self.parameters():
+    #             param.requires_grad = requires_grad
+    #     return self 
